0f240e22e1d6a3eda519f03bf5403a2b666bd4e2
Jiaxuan Lu <Jiaxuan.Lu@chicagobooth.edu>
2021-09-14 11:05:43 -0500

    isogram_JL

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
 def is_isogram(string):
-    pass
+    string0 = string.replace(" ","").replace("-","")
+    string0 = string0.lower()
+    if len(set(string0)) == len(string0):
+        return True
+    else:
+        return False
