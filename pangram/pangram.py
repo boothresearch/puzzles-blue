bf944c7453d8875d24dc5ad731b4cd62f42fca5f
Vivian <zjiuhe0615@gmail.com>
2021-09-14 11:22:54 -0500

    pangram finished

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,15 @@
+import re
+import string
 def is_pangram(sentence):
-    pass
+    sentence = sentence.lower()
+    sentence1 = re.sub(r'\W*', "", sentence)
+    sentence2 = re.sub(r'\d*', "", sentence1)
+    sentence3 = re.sub(r'_', "", sentence2)
+    character_list = [c for c in string.ascii_lowercase]
+    occurence = dict.fromkeys(character_list, 0)
+    for char in sentence3:
+        occurence[char] += 1
+    for val in occurence.values():
+        if val == 0:
+            return False
+    return True
