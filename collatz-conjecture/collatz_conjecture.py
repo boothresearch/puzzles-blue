b347c940aabeaeaa3829fc09c443e8313e93038f
Federico Marciano <federico.marciano@chicagobooth.edu>
2021-09-14 10:55:36 -0500

    fm-third-puzzle-solved

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
 def steps(number):
-    pass
+    if number<1 or number-round(number)>0: 
+        return print("error")
+    else:
+        if number % 2 == 0 : 
+            number= number/2 
+        elif number==1: 
+            number=number
+        else: 
+            number=3*number+1
+        print(str(number))
+        if number==1: 
+            return  1
+        else: 
+            return steps(number)
