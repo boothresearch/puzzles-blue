08491ece12c51e3df198135a519bdd4bd76d7867
Jiaxuan Lu <Jiaxuan.Lu@chicagobooth.edu>
2021-09-14 10:39:20 -0500

    anagram_JL

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,10 @@
 def find_anagrams(word, candidates):
-    pass
+    s = [i.lower() for i in word]
+    s.sort()
+    return_list = []
+    for candidate_word in candidates:
+        candidate_word_s = [i.lower() for i in candidate_word]
+        candidate_word_s.sort()
+        if (s == candidate_word_s) & (word.lower() != candidate_word.lower()):
+            return_list.append(candidate_word)
+    return(return_list)
