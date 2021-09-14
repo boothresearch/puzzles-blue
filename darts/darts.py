767d94eee2f1f51d2d9c2635255692db45f240bf
Vivian <zjiuhe0615@gmail.com>
2021-09-14 10:17:44 -0500

    finished darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,13 @@
+import math
 def score(x, y):
-    pass
+    dist = math.dist([x, y], [0,0])
+    if dist > 10:
+        return 0
+    elif dist == 10:
+        return 1
+    elif dist <= 10 and dist > 5: 
+        return 1
+    elif dist <= 5 and dist > 1:
+        return 5
+    elif dist <= 1:
+        return 10
