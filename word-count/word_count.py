5338297648465562d9c60dda584ee74beadd886d
Federico Marciano <federico.marciano@chicagobooth.edu>
2021-09-14 11:21:07 -0500

    word_counts

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,6 @@
 def count_words(sentence):
-    pass
+    import re 
+    sentence=re.sub(r"[!\?\.]", "", sentence )
+    sentence=sentence.lower().split()
+    wordfreq = [sentence.count(p) for p in sentence]
+    return dict(list(zip(sentence,wordfreq)))
