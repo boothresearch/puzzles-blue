4a82de6eb5654dda9545cfd74465fb09b4255f4d
Vivian <zjiuhe0615@gmail.com>
2021-09-14 10:56:21 -0500

    acronym finished

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,13 @@
 def abbreviate(words):
-    pass
+    words = words.replace("-", " ")
+    words = words.replace("_", "")
+    word_list = words.split()
+    for w in word_list:
+        if w == " ":
+            word_list.remove(w)
+    letters = [word[0] for word in word_list]
+    acro = ""
+    for l in letters:
+         l = l.upper()
+         acro = acro + l
+    return acro
